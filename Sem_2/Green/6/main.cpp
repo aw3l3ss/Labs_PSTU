426cc35b42e981330aa7f082b95cc81b8482dd3f
aw3l3ss <ivanshadrin066@gmail.com>
2024-04-11 19:19:52 +0500

    Add 6th task

---
+++ Sem_2/Green/6/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstring>
+
+#define MAX_STR_LEN 255
+
+using namespace std;
+
+void Gets(char **string);
+void task(char *str);
+
+int main()
+{
+    char *str = NULL;
+    Gets(&str);
+    task(str);
+    free(str);
+}
+
+void Gets(char **string) {
+    if (*string) {
+        free(*string);
+    }
+
+    char *str = (char *) calloc(MAX_STR_LEN + 1, sizeof(char));
+
+    char ch = getchar();
+    int i = 0;
+    while (ch != '\n') {
+        str[i] = ch;
+        i++;
+
+        ch = getchar();
+    }
+    str[i] = '\0';
+
+    *string = str;
+}
+
+void task(char *str) {
+    char *word = strtok(str, " ");
+
+    while (word != NULL) {
+        int cnt = 0;
+
+        for (int i = 0; i < strlen(word); i++) {
+            if (isdigit(word[i])) {
+                cnt++;
+            }
+        }
+
+        if (cnt == 1) {
+            cout << word << " ";
+        }
+
+        word = strtok(NULL, " ");
+    }
+
+    cout << endl;
+}
