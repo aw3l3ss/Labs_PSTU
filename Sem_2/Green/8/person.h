86d336e60597981426144d2a0b5bf880df767c9b
aw3l3ss <72498226+aw3l3ss@users.noreply.github.com>
2024-04-16 16:28:03 +0500

    Create person.h

---
+++ Sem_2/Green/8/person.h
@@ -0,0 +1,16 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <iostream>
+
+typedef struct Person Person;
+
+Person *create_person();
+void add_data_to_person(Person *person, std::string name, std::string adress, std::string phone, int age);
+std::string get_name(Person *person);
+std::string get_adress(Person *person);
+std::string get_phone(Person *person);
+int get_age(Person *person);
+void delete_person(Person *person);
+
+#endif
